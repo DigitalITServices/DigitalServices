7b0e797e33220a9a3b3b6ada5a48f593f9daad0d
Kerry Kidd <kerry@raspikidd.com>
2024-06-18 14:10:19 +0100

    Updated form

--- pages/HireMe.vue
+++ pages/HireMe.vue
@@ -55,48 +55,90 @@
         </div>
       </div>
       <!-- Contact Form -->
-      <form name="contact" method="post" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact">
+      <form 
+        class="mx-auto mt-16 max-w-xl sm:mt-20" 
+        name="contact" 
+        method="POST" 
+        data-netlify="true"
+        data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+		      <label>Don’t fill this out: <input name="bot-field" /></label>
+	      </p>
         <div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
           <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
             <div>
               <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">First name</label>
               <div class="mt-2.5">
-                <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+                <input 
+                  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                  id="first-name"  
+                  name="first-name" 
+                  type="text"
+                  autocomplete="text"  
+                  required 
+                />
                 <p class="text-red-500 text-xs italic"> Required </p>
               </div>
             </div>
             <div>
               <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
               <div class="mt-2.5">
-                <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+                <input
+                 class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                 id="last-name"
+                 name="last-name" 
+                 type="text" 
+                 autocomplete="text"  
+                 required />
                 <p class="text-red-500 text-xs italic"> Required </p>
               </div>
             </div>
             <div class="sm:col-span-2">
               <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
               <div class="mt-2.5">
-                <input type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+                <input
+                 class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                 id="email"  
+                 name="email" 
+                 type="email"
+                 autocomplete="email"  
+                 required />
                 <p class="text-red-500 text-xs italic"> Required </p>
               </div>
             </div>
             <div class="sm:col-span-2">
               <label for="phone-number" class="block text-sm font-semibold leading-6 text-gray-900">Phone number</label>
               <div class="mt-2.5">
-                <input type="tel" name="phone-number" id="phone-number" autocomplete="tel" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                <input 
+                 class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                 id="phone-number"  
+                 name="phone-number" 
+                 type="tel"
+                 autocomplete="text"  />
               </div>
             </div>
             <div class="sm:col-span-2">
               <label for="Service" class="block text-sm font-semibold leading-6 text-gray-900"> Service you are enquiring about </label>
               <div class="mt-2.5">
-                <input type="text" name="service" id="service" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+                <input 
+                 class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                 id="service"  
+                 name="service" 
+                 type="text"
+                 required />
                 <p class="text-red-500 text-xs italic"> Required </p>
               </div>
             </div>
             <div class="sm:col-span-2">
               <label for="message" class="block text-sm font-semibold leading-6 text-gray-900">Message</label>
               <div class="mt-2.5">
-                <textarea name="message" id="message" rows="4" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+                <textarea 
+                 class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                 name="message" 
+                 id="message" 
+                 rows="4"  
+                 required />
                 <p class="text-red-500 text-xs italic"> Required </p>
               </div>
             </div>
