d785a573d33f4df2741e8b534ea0acdc7281fbe1
Kerry Kidd <kerry@raspikidd.com>
2024-06-24 15:10:37 +0100

    Updated Contact form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -40,6 +40,32 @@
               </div>
             </div>
             <div class="sm:col-span-2">
+              <label for="company" class="block text-sm font-semibold leading-6 text-gray-900">Company Name</label>
+              <div class="mt-2.5">
+                <input
+                 class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                 id="company"  
+                 name="company" 
+                 type="text"
+                 autocomplete="organization"  
+                 required />
+                <p class="text-red-500 text-xs italic"> Required </p>
+              </div>
+            </div>
+            <div class="sm:col-span-2">
+              <label for="company-role" class="block text-sm font-semibold leading-6 text-gray-900">Company Role</label>
+              <div class="mt-2.5">
+                <input
+                 class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                 id="company-role"  
+                 name="company-role" 
+                 type="text"
+                 autocomplete="organization-title"  
+                 required />
+                <p class="text-red-500 text-xs italic"> Required </p>
+              </div>
+            </div>
+            <div class="sm:col-span-2">
               <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
               <div class="mt-2.5">
                 <input
