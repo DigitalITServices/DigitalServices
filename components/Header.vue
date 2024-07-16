fcf77ced1fbcf7c8e7561cb1be516c84a1476304
Kerry Kidd <kerry@raspikidd.com>
2024-07-16 14:53:07 +0100

    Updated header

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,10 @@
 <template>
-    
       <div class="relative bg-white shadow">
         <div class="max-w-7xl mx-auto px-4 sm:px-6">
           <div class="flex justify-between items-center py-6 md:justify-start md:space-x-10">
             <div class="flex justify-start lg:w-0 lg:flex-1">
               <NuxtLink to="/">
-                <span class="sr-only">Workflow</span>
+                <span class="sr-only">RasiKidd - Dream Digital</span>
                 <img class="h-8 w-auto sm:h-20" src="/images/RaspiKidd.svg" alt="Logo">
               </NuxtLink>
             </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,44 +13,7 @@ export default defineNuxtConfig({
 
   modules: [
     '@nuxtjs/tailwindcss', 
-   // "@weareheavy/nuxt-cookie-consent"
   ],
-
-  /* cookieConsent: {
-    provider: 'cookiescript',
-    id: 'a8d3c8226c608440504943f60d0dd3a9', // Replace with you "id" from CookieScript
-    dev: true,
-     scripts: {
-      necessary: [
-        {
-          id: 'necessary',
-          innerHTML: 'console.log("necessary")',
-          type: 'text/javascript',
-        },
-      ],
-      functional: [
-        {
-          id: 'functional',
-          innerHTML: 'console.log("functional")',
-          type: 'text/javascript',
-        },
-      ],
-      statistic: [
-        {
-          id: 'statistic',
-          innerHTML: 'console.log("statistic")',
-          type: 'text/javascript',
-        },
-      ],
-      marketing: [
-        {
-          id: 'marketing',
-          innerHTML: 'console.log("marketing")',
-          type: 'text/javascript',
-        },
-      ],
-    }, 
-  }, */
 
   nitro: {
     prerender: {
@@ -66,6 +29,3 @@ export default defineNuxtConfig({
     },
   },
 })
-
-/*<!-- Start cookieyes banner --> <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/d6f0c1a00dd0dc1726c250ea/script.js">
-</script>*/
