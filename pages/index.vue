11235693850d1f840c66aeff446c5bc39e7ea20e
Kerry Kidd <kerry@raspikidd.com>
2025-06-10 12:28:42 +0100

    Header updated and meeting WCAG criteria for colour contrast

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 
 <!--import CookieBanner from '~/components/CookieBanner.vue'; -->
 <template>
-    <div class="relative bg-gray-50">
+    <div class="relative bg-zinc-100">
         <Header />
         <CookieBanner />
     <main class="lg:relative">
     <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-30 lg:text-center">
       <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
         <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-          <span class="block text-red-700 xl:inline">Dream Digital </span> <br>
-          <span class="block xl:inline xl:text-5xl">All your Digital needs <br> in one place</span>
+          <span class="block text-sky-900 xl:inline">Dream </span> 
+          <span class="block text-green-900 xl:inline">Digital </span> <br>
+          <span class="block text-sky-600 xl:inline xl:text-5xl">Making tech easy </span> <br>
+          <span class="block text-purple-400 xl:inline xl:text-5xl">for everyday founders</span>
         </h1>
         
        <!--<div class="grid grid-cols-2 grid-rows-2 gap-2 place-items-auto h-30 mt-10 sm:ml-3">-->
