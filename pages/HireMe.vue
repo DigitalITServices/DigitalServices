3a5d3f84f2d82b0c4f07742f8d063f30b5ecf57b
Kerry Kidd <kerry@raspikidd.com>
2025-07-02 19:17:06 +0100

    Refactor ContactForm component for improved structure and validation; add new ContactFormNew component with enhanced features and service selection

--- pages/HireMe.vue
+++ pages/HireMe.vue
@@ -1,7 +1,7 @@
 <template>
   <Header />
   
-  <div class="relative isolate bg-zinc-100">
+  <div class="relative isolate bg-gray-100">
     <div class="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
       <div class="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
         <div class="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
@@ -61,21 +61,21 @@
    
   <!-- Testimonials -->
   <section class="bg-zinc-100">
-    <div class="max-w-7xl mx-auto md:grid md:grid-cols-1 md:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto md:grid md:grid-cols-1 md:px-6 lg:px-8 p-6">
       <br> <h2 class="text-4xl font-extrabold tracking-tight sm:text-4xl text-violet-500">
         Testimonials
       </h2>
     </div>
       
-    <div class="max-w-7xl mx-auto md:grid md:grid-cols-2 md:px-6 lg:px-8">
-      <div class="py-12 px-4 sm:px-6 md:flex md:flex-col md:py-16 md:pl-0 md:pr-10 md:border-r md:border-grey-400 lg:pr-16">
+    <div class="max-w-7xl mx-auto md:grid md:grid-cols-2 md:px-6 lg:px-8 p-4">
+      <div class="px-4 sm:px-6 md:flex md:flex-col md:py-16 md:pl-0 md:pr-10 md:border-r md:border-grey-400 lg:pr-16">
         <blockquote class="mt-6 md:flex-grow md:flex md:flex-col">
           <div class="relative text-lg font-medium text-gray-700 md:flex-grow">
             <svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-sky-200" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
               <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
             </svg>
     
-            <p class="relative">
+            <p class="mt-6 relative">
               Kerry has been a life saver in several instances for both my personal and business IT needs. Among other things she has helped me with:
             </p>
 
@@ -149,37 +149,47 @@
         </blockquote>
       </div>
         
-      <!-- <div class="py-12 px-4 border-t-2 border-red-600 sm:px-6 md:py-16 md:pr-0 md:pl-10 md:border-t-0 md:border-l lg:pl-16">
+      <div class="py-12 px-4 border-t-2 border-sky-400 sm:px-6 md:py-16 md:pr-0 md:pl-10 md:border-t-0 md:border-l lg:pl-16">
         <blockquote class="mt-6 md:flex-grow md:flex md:flex-col">
           <div class="relative text-lg font-medium text-black md:flex-grow">
-            <svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-red-300" fill="currentColor" viewBox="0 0 32 32">
+            <svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-sky-200" fill="currentColor" viewBox="0 0 32 32">
               <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
             </svg>
           
-            <p class="relative">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis. Nemo expedita voluptas culpa sapiente alias molestiae.
+            <p class="mt-6 relative">
+              Kerry is a talented and generous professional who makes computing and coding truly fun and accessible for children. Through her RaspiKidd coding clubs, she’s inspiring the 
+              next generation of digital thinkers — all while giving her time voluntarily. As a fellow committee member of Perthshire Business Women’s Network, I’ve also seen how Kerry 
+              supports others behind the scenes. 
+            </p>
+            <p class="mt-6  relative">
+              She helps technophobes like me with graphic design and technical tasks, always with patience and clarity. Kerry recently helped me bring my brand to life through a stunning 
+              laser-etched wooden sign for my business, Later Life Concierge. Working alongside Scott at the Famous Grouse Ideas Centre, she managed the technical and design work 
+              beautifully. The finished piece is not only eye-catching but has become a standout feature in all my presentations. 
+            </p>
+            <p class="mt-6  relative">
+              Kerry’s creativity, skill, and easygoing nature make her an invaluable asset to her varied business talents— and I recommend her without hesitation. 
             </p>
           </div>
         
           <footer class="mt-8">
             <div class="flex items-start">
-              <div class="flex-shrink-0 inline-flex rounded-full border-2 border-red-500">
-                <img class="h-12 w-12 rounded-full" src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixqx=kMf6WlMMZH&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+              <div class="flex-shrink-0 inline-flex rounded-full border-2 border-sky-500">
+                <img class="h-12 w-12 rounded-full" src="https://images.squarespace-cdn.com/content/v1/610da7a23d2f4951875769fd/509fd786-9018-4534-b881-5c95c7511ea2/colour+small+online.png?format=1500w" alt="Later Life Concierge Logo">
               </div>
                 
               <div class="ml-4">
                 <div class="text-base font-medium text-black">
-                  Joseph Rodriguez
+                  Pauline
                 </div>
                   
-                <div style ="color:#D02530;" class="text-base font-medium text-red-500">
-                  CEO, Workcation
+                <div class="text-base font-medium text-sky-700">
+                  Founder, Later Life Concierge
                 </div>
               </div>
             </div>
           </footer>
         </blockquote>
-      </div> -->
+      </div>
     </div>
   </section>
 
