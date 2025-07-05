92b0454adb86ebad2600a98e4efa9534d1ed0208
Kerry Kidd <kerry@raspikidd.com>
2025-07-05 18:36:58 +0100

    feat: add Cookie Policy page with detailed cookie usage information

---
+++ pages/CookiePolicy.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class = "mx-auto mt-32 max-w-7xl px-6 sm:mt-20 lg:px-8">
+    <h1 class="cookie-policy-h1 text-pretty text-3xl font-semibold tracking-tight text-sky-700 sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl">
+        Cookie Policy
+    </h1>
+<p class="text-xl/8 text-gray-700 font-semibold">
+    Effective Date: 03-Jul-2025 <br>
+    Last Updated: 03-Jul-2025
+</p>
+
+&nbsp;
+<h2 class="mt-5 text-pretty text-4xl font-semibold tracking-tight text-violet-700 sm:text-5xl">
+    What are cookies?
+</h2>
+<div class="cookie-policy-p">
+    <p class="text-xl/8 text-gray-700 font-semibold">
+        This Cookie Policy explains what cookies are and how we use them, the types of cookies we use i.e, the information we collect using cookies and 
+        how that information is used, and how to manage the cookie settings.
+    </p> 
+    <p class="text-xl/8 text-gray-700 font-semibold">
+        Cookies are small text files that are used to store small pieces of information. They are stored on your device when the website is loaded on 
+        your browser. These cookies help us make the website function properly, make it more secure, provide better user experience, and understand how 
+        the website performs and to analyze what works and where it needs improvement.
+    </p>
+</div>
+&nbsp;
+<h2 class="mt-5 text-pretty text-4xl font-semibold tracking-tight text-violet-700 sm:text-5xl">
+    How do we use cookies?
+</h2>
+<div class="cookie-policy-p">
+    <p class="text-xl/8 text-gray-700 font-semibold">
+        As most of the online services, our website uses first-party and third-party cookies for several purposes. First-party cookies are mostly 
+        necessary for the website to function the right way, and they do not collect any of your personally identifiable data.
+    </p> 
+    <p class="text-xl/8 text-gray-700 font-semibold">
+        The third-party cookies used on our website are mainly for understanding how the website performs, how you interact with our website, keeping 
+        our services secure, providing advertisements that are relevant to you, and all in all providing you with a better and improved user experience 
+        and help speed up your future interactions with our website.
+    </p>
+</div>
+&nbsp;
+<h2 class="mt-5 text-pretty text-4xl font-semibold tracking-tight text-violet-700 sm:text-5xl">
+    Types of Cookies we use
+</h2>
+
+    <div class="cky-audit-table-element">
+        
+    </div>
+
+&nbsp;
+    <h2 class="mt-5 text-pretty text-4xl font-semibold tracking-tight text-violet-700 sm:text-5xl">
+        Manage cookie preferences
+    </h2>
+
+    <a class="cky-banner-element">Cookie Settings</a> <br/>
+
+    <div>
+        <p class="text-xl/8 text-gray-700 font-semibold">
+            You can change your cookie preferences any time by clicking the above button. This will let you revisit the cookie consent banner and 
+            change your preferences or withdraw your consent right away. 
+        </p> 
+        <p class="text-xl/8 text-gray-700 font-semibold">
+            In addition to this, different browsers provide different methods to block and delete cookies used by websites. You can change the 
+            settings of your browser to block/delete the cookies. Listed below are the links to the support documents on how to manage and delete 
+            cookies from the major web browsers.
+        </p> 
+        <p class="text-xl/8 text-gray-700 font-semibold">
+            Chrome: 
+            <a class = "hover:text-sky-700" target="_blank" href="https://support.google.com/accounts/answer/32050">
+                https://support.google.com/accounts/answer/32050
+            </a>
+        </p>
+        <p class="text-xl/8 text-gray-700 font-semibold">
+            Safari: 
+            <a class = "hover:text-sky-700" target="_blank" href="https://support.apple.com/en-in/guide/safari/sfri11471/mac">
+                https://support.apple.com/en-in/guide/safari/sfri11471/mac
+            </a>
+        </p>
+        <p class="text-xl/8 text-gray-700 font-semibold">
+            Firefox: 
+            <a class = "hover:text-sky-700" target="_blank" href="https://support.mozilla.org/en-US/kb/clear-cookies-and-site-data-firefox?redirectslug=delete-cookies-remove-info-websites-stored&redirectlocale=en-US">
+                https://support.mozilla.org/en-US/kb/clear-cookies-and-site-data-firefox?redirectslug=delete-cookies-remove-info-websites-stored&redirectlocale=en-US
+            </a>
+        </p>
+        <p class="text-xl/8 text-gray-700 font-semibold">
+            Internet Explorer: 
+            <a class = "hover:text-sky-700" target="_blank" href="https://support.microsoft.com/en-us/topic/how-to-delete-cookie-files-in-internet-explorer-bca9446f-d873-78de-77ba-d42645fa52fc">
+                https://support.microsoft.com/en-us/topic/how-to-delete-cookie-files-in-internet-explorer-bca9446f-d873-78de-77ba-d42645fa52fc
+            </a>
+        </p>
+        <p class="text-xl/8 text-gray-700 font-semibold">
+            If you are using any other web browser, please visit your browser’s official support documents.
+        </p>
+    </div>
+&nbsp;
+<p class="cookie-policy-p text-xl/8 text-gray-700 font-semibold">
+    Cookie Policy Generated By  
+    <a class = "hover:text-sky-700" target="_blank" href="https://www.cookieyes.com/?utm_source=CP&utm_medium=footer&utm_campaign=UW">
+        CookieYes - Cookie Policy Generator
+    </a>.
+</p>
+</div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+    a.cky-banner-element {
+        padding: 8px 30px;
+        background: #F8F9FA;
+        color: #858A8F;
+        border: 1px solid #DEE2E6;
+        box-sizing: border-box;
+        border-radius: 2px;
+        cursor: pointer;
+}
+</style>
