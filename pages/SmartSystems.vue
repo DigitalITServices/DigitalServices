002373fcb1755e0f12e0546d0f4b9fd377343d51
Kerry Kidd <kerry@raspikidd.com>
2025-08-06 14:46:21 +0100

    feat: update call-to-action text and link on Smart Systems page

--- pages/SmartSystems.vue
+++ pages/SmartSystems.vue
@@ -159,9 +159,10 @@
                 <i>
                     Transform your business from reactive to proactive. Upgrade to Smart Systems Setup and experience the future of business operations today!
                 </i>
-                <br />
-                <a href="https://app.simplymeet.me/dream-digital/dream-digital-30-1751571285886" class="rounded-md bg-violet-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-violet-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600" target="_blank">
-                    Book A Call
+            </p>
+            <p class="mt-6 center">
+                <a href="https://app.simplymeet.me/dream-digital/smart-systems" class="rounded-md bg-violet-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-violet-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600" target="_blank">
+                    Build My Automated Empire
                 </a>
             </p>
         </div>
