6cce78318905bdecb44140fca855020183919697
Kerry Kidd <kerry@raspikidd.com>
2026-03-30 17:05:38 +0100

    feat: update header component styles for improved UI consistency

--- components/Header.vue
+++ components/Header.vue
@@ -51,9 +51,9 @@
               </div>
             </div>
         <!-- Desktop Menu END -->
-      </div>
-    <!-- Mobile Menu -->
-    <div class="absolute top-0 inset-x-0 z-10 p-2 transition transform origin-top-right md:hidden">
+        </div>
+        <!-- Mobile Menu -->
+        <div class="absolute top-0 inset-x-0 z-10 p-2 transition transform origin-top-right md:hidden">
               <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
                 <div class="pt-5 pb-6 px-5">
                   <div class="flex items-center justify-between">
@@ -69,8 +69,8 @@
                     </NuxtLink>
                     <div class="flex items-center">
                       <div class="flex-shrink-0">
-                        <a href="https://app.simplymeet.me/dream-digital/discovery-call" target="_blank" class="relative inline-flex items-center gap-x-1.5 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-500">
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#7bae7f" class="size-6">
+                        <a href="https://app.simplymeet.me/dream-digital/discovery-call" target="_blank" class="relative inline-flex items-center gap-x-1.5 rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-500">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#38bdf8" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
                           </svg>
                           Book a Call
@@ -78,7 +78,7 @@
                       </div>
                     </div>
                     <div class="-mr-2">
-                      <button type="button" @click="isOpen = !isOpen" class="rounded-md p-2 inline-flex items-center justify-center hover:text-gray-600 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-600">
+                      <button type="button" @click="isOpen = !isOpen" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                         <span class="sr-only">Open menu</span>
                         <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -93,7 +93,7 @@
                         leave-to-class="opacity-0 scale-95"
                       >
                         <div v-show="isOpen" class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg">
-                          <div class="rounded-md shadow-xs">
+                          <div class="rounded-md bg-white shadow-xs">
                             <div class="mt-6">
                               <nav class="grid gap-y-8">
                                 <!--<NuxtLink to="/WebDesign" class="rounded-t bg-white hover:bg-gray-400 -m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
@@ -151,25 +151,25 @@
                                   </div>
                                 </NuxtLink> -->
 
-                                <NuxtLink to="/services" class="text-base font-medium hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-300">
+                                <NuxtLink to="/services" class="text-base font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-300">
                                   <div class="ml-4">
-                                    <p class="text-base font-medium">
+                                    <p class="text-base font-medium text-gray-600">
                                       Services
                                     </p>
                                   </div>
                                 </NuxtLink>
           
-                                <NuxtLink to="/about" class="text-base font-medium hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-300">
+                                <NuxtLink to="/about" class="text-base font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-300">
                                   <div class="ml-4">
-                                    <p class="text-base font-medium">
+                                    <p class="text-base font-medium text-gray-600">
                                       About
                                     </p>
                                   </div>
                                 </NuxtLink>
   
-                                 <NuxtLink to="/hire-me" class="text-base font-medium hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-300">
+                                 <NuxtLink to="/hire-me" class="text-base font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-300">
                                   <div class="ml-4">
-                                    <p class="text-base font-medium">
+                                    <p class="text-base font-medium text-gray-600">
                                       Hire Us
                                     </p>
                                   </div>
